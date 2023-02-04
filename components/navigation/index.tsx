fc2a54ad212e1dd9f92a2c496e5f6c37f58bca40
Emrah Atalay <emrahatalay92@gmail.com>
2023-02-04 12:52:18 +0300

    build dir eklendi.

--- components/navigation/index.tsx
+++ components/navigation/index.tsx
@@ -4,9 +4,9 @@ function Navigation(): JSX.Element {
     return (
         <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
             <div className="container flex flex-wrap items-center justify-between mx-auto">
-                <a href="https://eatalay.dev/" className="flex items-center">
+                <Link href="https://eatalay.dev/" className="flex items-center">
                     <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Emrah Atalay</span>
-                </a>
+                </Link>
                 <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="mobile-menu-2">
                     <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                         <li>
