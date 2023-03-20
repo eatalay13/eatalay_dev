0d5565d2d139a190b016638354bfe6815353b037
Emrah Atalay <emrahatalay92@gmail.com>
2023-03-20 13:43:36 +0300

    Dosyalar src klasörü altına taşındı.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,8 @@ module.exports = {
   mode: "jit",
   content: [
     "./public/**/*.html",
-    "./app/**/*.{js,jsx,ts,tsx,vue}",
-    "./components/**/*.{js,jsx,ts,tsx,vue}",
+    "./src/app/**/*.{js,jsx,ts,tsx,vue}",
+    "./src/components/**/*.{js,jsx,ts,tsx,vue}",
     "./public/**/*.{js,jsx,ts,tsx,vue}",
   ],
   theme: {
