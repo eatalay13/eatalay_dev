92a06971a1bf06dab4209fedf4cc91aad2fa7e6c
Emrah Atalay <emrahatalay92@gmail.com>
2024-06-10 18:48:31 +0300

    chore: Add Button component for homepage

---
+++ src/components/button/index.tsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.css';
+
+function Button() {
+    return (
+        <button className={styles.base}>
+            Click me
+        </button>
+    )
+}
+
+export default Button;
