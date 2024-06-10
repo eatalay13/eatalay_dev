563f984277ea8e421dbb6f849b84eb68f5cc4a4b
Emrah Atalay <emrahatalay92@gmail.com>
2024-06-10 14:16:22 +0300

    Reset project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,24 +1,20 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  darkMode: "class",
   theme: {
-    screens: {
-      xs: '320px',
-      sm: '576px',
-      md: "960px",
-    },
     extend: {
-      fontFamily: {
-        title: "var(--font-poppins)",
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
