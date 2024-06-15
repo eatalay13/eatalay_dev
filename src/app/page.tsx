72a59f52500d08cc7b651a881f183eb35edd9273
Emrah Atalay <emrahatalay92@gmail.com>
2024-06-15 10:01:28 +0300

    feat: Add TypewriterComponent for dynamic text display
    The TypewriterComponent has been added to the page.tsx file to enable dynamic text display. This component allows for a typewriter effect, showcasing different strings of text. It enhances the visual appeal and user experience of the website.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import TypewriterComponent from "@/components/type-writer";
 import { PrismaClient } from "@prisma/client";
 import Image from "next/image";
 
@@ -19,7 +20,7 @@ async function Home() {
           width={50}
           height={50}
         />
-        Next.js + Prisma
+        <TypewriterComponent strings={["Frond-end", "Back-end", "Mobile", "Game Developer"]} />
       </h1>
       <p className="text-center mt-4">Welcome, {user?.name}!</p>
       <p className="text-center mt-2 text-xs">Email: {user?.email}</p>
