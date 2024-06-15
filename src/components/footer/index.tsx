af7f8dc5aefce9c8a79098752e69d11ee50cd23b
Emrah Atalay <emrahatalay92@gmail.com>
2024-06-15 10:00:53 +0300

    feat: Update social media links in Footer component
    The social media links in the Footer component have been updated to include the correct URLs for GitHub, LinkedIn, Instagram, and Twitter. This ensures that users can easily navigate to the developer's social media profiles.

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -15,17 +15,17 @@ function Footer() {
                     Made with ❤️
                 </span>
                 <div className='flex mt-4 space-x-6 sm:justify-center sm:mt-0'>
-                    <a className="text-gray-500 hover:text-gray-900 dark:hover:text-white" href="" target='_blank'>
+                    <a className="text-gray-500 hover:text-gray-900 dark:hover:text-white" href="https://github.com/eatalay13" target='_blank'>
                         <Github size={16} />
                     </a>
-                    <a className="text-gray-500 hover:text-gray-900 dark:hover:text-white" href="" target='_blank'>
+                    <a className="text-gray-500 hover:text-gray-900 dark:hover:text-white" href="https://www.linkedin.com/in/emrah-atalay-934919184" target='_blank'>
+                        <Linkedin size={16} />
+                    </a>
+                    <a className="text-gray-500 hover:text-gray-900 dark:hover:text-white" href="https://instagram.com/emrhatalay" target='_blank'>
                         <Instagram size={16} />
                     </a>
-                    <a className="text-gray-500 hover:text-gray-900 dark:hover:text-white" href="" target='_blank'>
+                    <a className="text-gray-500 hover:text-gray-900 dark:hover:text-white" href="https://x.com/emrhatalay" target='_blank'>
                         <Twitter size={16} />
-                    </a>
-                    <a className="text-gray-500 hover:text-gray-900 dark:hover:text-white" href="" target='_blank'>
-                        <Linkedin size={16} />
                     </a>
                 </div>
             </div>
