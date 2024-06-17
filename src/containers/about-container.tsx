c4a134a32f1ba03d4fd15dcfef3ddbaa2fc67c0b
Emrah Atalay <emrahatalay92@gmail.com>
2024-06-17 05:20:14 +0300

    feat: Update mobile layout and add lazy loading for images
    The mobile layout in the RootLayout component has been improved by centering the content using a flex container. Additionally, lazy loading has been implemented for the images in the BlogPage component. These enhancements improve the user experience by providing a visually appealing and centered layout on mobile devices, as well as optimizing page load times by loading images only when necessary.

--- src/containers/about-container.tsx
+++ src/containers/about-container.tsx
@@ -2,60 +2,12 @@ import Image from 'next/image'
 
 function AboutContainer() {
     return (
-        <div className="flex flex-col items-center justify-center h-full">
-            <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-                <Image className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/images/pp-bw.jpeg" alt="Profile " width="384" height="512" />
-                <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
-                    <blockquote>
-                        <p className="text-lg font-medium">
-                            Emrah is a talented developer who is always eager to learn new things. He is a great team player and always willing to help others. He is a great asset to any team
-                        </p>
-                    </blockquote>
-                    <figcaption className="font-medium">
-                        <div className="text-sky-500 dark:text-sky-400">
-                            Emrah Atalay
-                        </div>
-                        <div className="text-slate-700 dark:text-slate-500">
-                            Software Developer - Bitlis Turkey
-                        </div>
-                    </figcaption>
-                </div>
-            </figure>
-            <div className="flex justify-center mt-4 space-x-4">
-                <div className="text-gray-500 dark:text-gray-400">
-                    <a href="dsds" target="_blank">
-                        Github
-                    </a>
-                    <a href="dsds" target="_blank">
-                        Linkedin
-                    </a>
-                    <a href="dsds" target="_blank">
-                        Instagram
-                    </a>
-                    <a href="dsds" target="_blank">
-                        Twitter
-                    </a>
-                </div>
+        <div className="flex items-center justify-center">
+            <div className="flex flex-col items-center">
+                <Image src="/images/pp-bw.jpeg" alt="Emrah Atalay" className="rounded-full" width={250} height={250} />
+                <h1 className="text-2xl font-bold mt-4">Emrah Atalay</h1>
+                <p className="text-center mt-2">Full-stack Developer</p>
             </div>
-
-            <table className="table-auto">
-                <thead>
-                    <tr>
-                        <th>Title</th>
-                        <th>Author</th>
-                        <th>Views</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {Array.from({ length: 100 }).map((_, index) => (
-                        <tr key={index}>
-                            <td>Intro to CSS</td>
-                            <td>Adam</td>
-                            <td>858</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
         </div>
     )
 }
