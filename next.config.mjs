ffdb88adc750d14bce4194d6d860ed3c72a8918c
Emrah Atalay <emrahatalay92@gmail.com>
2024-06-18 22:25:30 +0300

    feat: Add next-pwa dependency for progressive web app functionality

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
+import pkg from "next-pwa";
+
+const withPWA = pkg({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  //disable: process.env.NODE_ENV === "development",
+});
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = withPWA({
+  reactStrictMode: false,
+  output: "standalone",
+  images: {
+    domains: ["images.unsplash.com"],
+  },
+});
 
 export default nextConfig;
