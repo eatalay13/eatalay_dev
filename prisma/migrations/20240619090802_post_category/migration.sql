e44530373a231e2fadc472cfd0d9ed23b6322b0a
Emrah Atalay <emrahatalay92@gmail.com>
2024-06-19 12:08:45 +0300

    feat: Add migration for PostCategory table and foreign key constraint
    This commit adds a migration file that creates the `PostCategory` table with columns for `id`, `name`, `order`, `createdAt`, and `updatedAt`. It also adds a foreign key constraint to the `Post` table, linking it to the `PostCategory` table via the `categoryId` column. This migration enhances the database schema by introducing a new table for managing post categories and establishing a relationship between posts and categories.

---
+++ prisma/migrations/20240619090802_post_category/migration.sql
@@ -0,0 +1,22 @@
+/*
+  Warnings:
+
+  - Added the required column `categoryId` to the `Post` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Post" ADD COLUMN     "categoryId" TEXT NOT NULL;
+
+-- CreateTable
+CREATE TABLE "PostCategory" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "order" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "PostCategory_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "PostCategory"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
