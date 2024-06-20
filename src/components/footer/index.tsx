987a5754f32125fd9501c84646669cea39077074
Emrah Atalay <emrahatalay92@gmail.com>
2024-06-20 11:09:48 +0300

    feat: Add @heroicons/react dependency and update package.json
    The @heroicons/react dependency has been added to the project with version 2.1.4. Additionally, the package.json file has been updated to include this new dependency. This change enhances the project by providing access to a library of customizable icons for use in the application.

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,10 +1,3 @@
-import {
-    Github,
-    Instagram,
-    Linkedin,
-    Twitter,
-} from 'lucide-react';
-
 import styles from "./styles.module.css";
 
 function Footer() {
@@ -16,16 +9,16 @@ function Footer() {
                 </span>
                 <div className='flex mt-4 space-x-6 sm:justify-center sm:mt-0'>
                     <a className="text-gray-500 hover:text-gray-900 dark:hover:text-white" href="https://github.com/eatalay13" target='_blank'>
-                        <Github size={16} />
+
                     </a>
                     <a className="text-gray-500 hover:text-gray-900 dark:hover:text-white" href="https://www.linkedin.com/in/emrah-atalay-934919184" target='_blank'>
-                        <Linkedin size={16} />
+
                     </a>
                     <a className="text-gray-500 hover:text-gray-900 dark:hover:text-white" href="https://instagram.com/emrhatalay" target='_blank'>
-                        <Instagram size={16} />
+
                     </a>
                     <a className="text-gray-500 hover:text-gray-900 dark:hover:text-white" href="https://x.com/emrhatalay" target='_blank'>
-                        <Twitter size={16} />
+
                     </a>
                 </div>
             </div>
