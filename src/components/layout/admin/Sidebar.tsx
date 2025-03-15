55dec4ab27c6a925753e4f875f9532d616d9a543
Emrah Atalay <emrahatalay92@gmail.com>
2025-03-16 02:13:10 +0300

    feat: Sidebar bileşeninde simgeler güncellendi ve stil iyileştirmeleri yapıldı
    - Sidebar bileşeninde simgeler, react-icons kütüphanesinden alınan bileşenlerle değiştirildi.
    - Aktif menü öğeleri için stil güncellemeleri yapıldı ve geçiş efektleri eklendi.
    - Kod okunabilirliği artırıldı ve gereksiz SVG kodları kaldırıldı.

--- src/components/layout/admin/Sidebar.tsx
+++ src/components/layout/admin/Sidebar.tsx
@@ -3,6 +3,15 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
+import { FaAppStore } from "react-icons/fa";
+import {
+  LuLayoutDashboard,
+  LuMails,
+  LuNotebookText,
+  LuTags,
+  LuUsers,
+} from "react-icons/lu";
+import { TbFilters } from "react-icons/tb";
 
 type MenuItem = {
   name: string;
@@ -14,152 +23,37 @@ const menuItems: MenuItem[] = [
   {
     name: "Dashboard",
     href: "/admin",
-    icon: (props) => (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        {...props}
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
-        />
-      </svg>
-    ),
+    icon: (props) => <LuLayoutDashboard {...props} size={18} />,
   },
   {
     name: "Bloglar",
     href: "/admin/blogs",
-    icon: (props) => (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        {...props}
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
-        />
-      </svg>
-    ),
+    icon: (props) => <LuNotebookText {...props} size={18} />,
   },
   {
     name: "Projeler",
     href: "/admin/projects",
-    icon: (props) => (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        {...props}
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 00.75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 00-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0112 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 01-.673-.38m0 0A2.18 2.18 0 013 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 013.413-.387m7.5 0V5.25A2.25 2.25 0 0013.5 3h-3a2.25 2.25 0 00-2.25 2.25v.894m7.5 0a48.667 48.667 0 00-7.5 0M12 12.75h.008v.008H12v-.008z"
-        />
-      </svg>
-    ),
+    icon: (props) => <FaAppStore {...props} size={18} />,
   },
   {
     name: "Kategoriler",
     href: "/admin/categories",
-    icon: (props) => (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        {...props}
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M9.568 3H5.25A2.25 2.25 0 003 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 005.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 009.568 3z"
-        />
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M6 6h.008v.008H6V6z"
-        />
-      </svg>
-    ),
+    icon: (props) => <TbFilters {...props} size={18} />,
   },
   {
     name: "Etiketler",
     href: "/admin/tags",
-    icon: (props) => (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        {...props}
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M9.568 3H5.25A2.25 2.25 0 003 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 005.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 009.568 3z"
-        />
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M6 6h.008v.008H6V6z"
-        />
-      </svg>
-    ),
+    icon: (props) => <LuTags {...props} size={18} />,
   },
   {
     name: "İletişim Mesajları",
     href: "/admin/contacts",
-    icon: (props) => (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        {...props}
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
-        />
-      </svg>
-    ),
+    icon: (props) => <LuMails {...props} size={18} />,
   },
   {
     name: "Kullanıcılar",
     href: "/admin/users",
-    icon: (props) => (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        {...props}
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z"
-        />
-      </svg>
-    ),
+    icon: (props) => <LuUsers {...props} size={18} />,
   },
 ];
 
@@ -233,20 +127,20 @@ export default function Sidebar() {
         <nav className="mt-5 px-2 space-y-1">
           {menuItems.map((item) => {
             const isActive =
-              pathname === item.href || pathname.startsWith(`${item.href}/`);
+              pathname === item.href || pathname === `${item.href}/`;
             return (
               <Link
                 key={item.name}
                 href={item.href}
                 onClick={handleClick}
-                className={`group flex items-center px-2 py-2 text-base font-medium rounded-md ${
+                className={`group flex items-center px-2 py-2 text-base font-medium rounded-md transition-all duration-300 ease-in-out ${
                   isActive
-                    ? "bg-yellow-500 text-white dark:bg-yellow-700"
+                    ? "bg-gradient-to-r from-indigo-500 to-purple-500 text-white dark:from-indigo-700 dark:to-purple-700 "
                     : "text-gray-300 hover:bg-zinc-800 hover:text-white"
                 }`}
               >
                 <item.icon
-                  className={`mr-3 flex-shrink-0 h-6 w-6 ${
+                  className={`mr-3 flex-shrink-0 ${
                     isActive
                       ? "text-white"
                       : "text-gray-400 group-hover:text-gray-300"
