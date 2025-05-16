1cfd41f2cd16a10fc3aafef0c97699401ef1a4f4
Emrah Atalay <emrahatalay92@gmail.com>
2025-05-16 17:35:40 +0300

    feat: Font bileşenleri eklendi; layout dosyasında iyileştirmeler yapıldı

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,20 +1,10 @@
 import { routing } from "@/i18n/routing";
 import "@/styles/globals.css";
+import { geistMono, geistSans } from "@/utils/fonts";
 import type { Metadata } from "next";
-import { hasLocale } from "next-intl";
+import { hasLocale, NextIntlClientProvider } from "next-intl";
 import { getLocale } from "next-intl/server";
-import { Geist, Geist_Mono } from "next/font/google";
 import { notFound } from "next/navigation";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Emrah Atalay",
@@ -28,15 +18,14 @@ type RootLayoutProps = {
 async function RootLayout({ children }: RootLayoutProps) {
   const locale = await getLocale();
 
-  if (!hasLocale(routing.locales, locale)) {
-    notFound();
-  }
+  if (!hasLocale(routing.locales, locale)) notFound();
+
   return (
     <html lang={locale} dir="ltr">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <NextIntlClientProvider>{children}</NextIntlClientProvider>
       </body>
     </html>
   );
